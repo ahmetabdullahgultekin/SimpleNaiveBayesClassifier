67ea33bf5bc7334f1d4abf96e69e4ae7845d6f14
Ahmet Abdullah Gültekin <ahabgu@gmail.com>
2024-11-14 23:59:08 +0300

    Project advanced

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 import json
 
+from panel.io.resources import json_dumps
+
 # Constant for the target column
 TARGET_COLUMN = 'PlayTennis'
+
 
 # Load and prepare your dataset
 def load_data(file_path):
@@ -14,23 +17,58 @@ def load_data(file_path):
     summarize_data(data)
     return data
 
+
 # Summarize the dataset
 def summarize_data(data):
     print('Unique values in each column:')
     for column in data.columns:
         print(f'{column}: {data[column].unique()}')
     print('\n---------------------------------------------------------')
-    summarize_occurrences(data)
+
 
 # Summarize the occurrences of each value in the target column
 def summarize_occurrences(data):
     target_value_counts = data[TARGET_COLUMN]
+    add_data_to_json(data, target_value_counts)
+    # print_values_from_json()
+    print_values_from_memory(target_value_counts, data)
+
+
+# Write the datas to the json file
+def add_data_to_json(data, target_value_counts):
+    # Open the json file in append mode
+    with open('values.json', 'a') as values_file:
+        values_file.write('{')
+        values_file.write('"Values": [')
+        values_file.write(target_value_counts.value_counts().to_json() + ',')
+        values_file.write(target_value_counts.value_counts(normalize=True).to_json() + ',')
+        for column in data.drop(columns=[TARGET_COLUMN]).columns:
+            values_file.write(data.groupby([column, TARGET_COLUMN]).size().to_json() + ',')
+            # Do not add comma at the end
+            #values_file.write((data.groupby([column, TARGET_COLUMN]).size() / data.groupby(TARGET_COLUMN).size()).to_json() + ',')
+            if column == data.drop(columns=[TARGET_COLUMN]).columns[-1]:
+                values_file.write((data.groupby([column, TARGET_COLUMN]).size() / data.groupby(TARGET_COLUMN).size()).to_json())
+            else:
+                values_file.write((data.groupby([column, TARGET_COLUMN]).size() / data.groupby(TARGET_COLUMN).size()).to_json() + ',')
+        values_file.write(']')
+        values_file.write('}')
+
+# Print the values from the json file
+def print_values_from_json():
+    with open('values.json', 'r') as values_file:
+        values = json.load(values_file)
+        print(values)
+
+
+# Print the values from the memory
+def print_values_from_memory(target_value_counts, data):
     print('Occurrences of each value in the target column:')
     print(target_value_counts.value_counts())
     print('\n---------------------------------------------------------')
     print('Occurrences of each value in the target column (normalized):')
     print(target_value_counts.value_counts(normalize=True))
     print('\n---------------------------------------------------------')
+
     # Summarize the occurrences of each value for Yes and No
     for column in data.drop(columns=[TARGET_COLUMN]).columns:
         print(f'Occurrences of each value in the {column} column:')
@@ -43,43 +81,49 @@ def summarize_occurrences(data):
 
 # Calculate prior probabilities
 def calculate_prior(data, target_column):
+    priors = {}
     priors = data[target_column].value_counts(normalize=True).to_dict()
     print('Priors:')
     print(priors)
     return priors
 
 
-# Calculate likelihoods with Laplace smoothing
-def calculate_likelihoods(data, target_column):
-    likelihoods = {}
-    for column in data.drop(columns=[target_column]).columns:
-        likelihoods[column] = {}
+# Calculate likelihoods
+def calculate_likelihoods(data):
+    likelihoods_dict = {}
+    for column in data.drop(columns=[TARGET_COLUMN]).columns:
+        likelihoods_dict[column] = {}
         for value in data[column].unique():
-            likelihoods[column][value] = {}
-            for target_value in data[target_column].unique():
-                count = len(data[(data[column] == value) & (data[target_column] == target_value)])
-                total = len(data[data[target_column] == target_value])
-                likelihoods[column][value][target_value] = (count + 1) / (total + len(data[column].unique()))
-    return likelihoods
+            likelihoods_dict[column][value] = {}
+            for target_value in data[TARGET_COLUMN].unique():
+                likelihoods_dict[column][value][target_value] = data.groupby(
+                    [column, TARGET_COLUMN]).size() / data.groupby(TARGET_COLUMN).size()
+    return likelihoods_dict
+
 
 # Train the Naive Bayes model
 def train_naive_bayes(data, target_column):
     priors = calculate_prior(data, target_column)
-    likelihoods = calculate_likelihoods(data, target_column)
+    likelihoods = calculate_likelihoods(data)
     return {'priors': priors, 'likelihoods': likelihoods}
+
 
 # Save the model as a JSON file
 def save_model(model, file_path):
     with open(file_path, 'w') as file:
         json.dump(model, file)
 
+
 # Load the model from JSON file
 def load_model(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
+
 # Predict class for a single instance
 def predict(instance, model):
+    # Get the values of the priors and likelihoods from the model belongs to the instance
+    model.get('priors')
     priors = model['priors']
     likelihoods = model['likelihoods']
     posteriors = {}
@@ -91,6 +135,7 @@ def predict(instance, model):
         posteriors[class_value] = posterior
     return max(posteriors, key=posteriors.get)
 
+
 # Evaluate the model on test data
 def evaluate_model(data, model, target_column):
     correct = 0
@@ -100,13 +145,22 @@ def evaluate_model(data, model, target_column):
             correct += 1
     return correct / len(data)
 
+
 # Start the program
 if __name__ == "__main__":
     data_set = load_data('play_tennis_dataset.csv')
-    #label_encoders, data_set = encode_data(data_set)
-    calculate_likelihoods(data_set, TARGET_COLUMN)
+    summarize_occurrences(data_set)
+    calculate_likelihoods(data_set)
     training_model = train_naive_bayes(data_set, TARGET_COLUMN)
-    #save_model(model, 'naive_bayes_model.json')
-    #loaded_model = load_model('naive_bayes_model.json')
-    #accuracy = evaluate_model(data, loaded_model, 'Wind')
-    #print(f'Accuracy: {accuracy}')
+    print(training_model)
+    new_prediction_data = {'Outlook': 'Sunny', 'Temperature': 'Cool', 'Humidity': 'High', 'Wind': 'Strong'}
+    #prediction = predict(new_prediction_data, training_model)
+    # add_data_to_json(training_model)
+    # save_model(model, 'naive_bayes_model.json')
+    # loaded_model = load_model('naive_bayes_model.json')
+    # accuracy = evaluate_model(data, loaded_model, 'Wind')
+    # print(f'Accuracy: {accuracy}')
+    # Predict a new instance
+    # new_instance = {'Outlook': 'Sunny', 'Temperature': 'Cool', 'Humidity': 'High', 'Wind': 'Strong'}
+    # prediction = predict(new_instance, model)
+    # print(f'Prediction for the new instance: {prediction}')
